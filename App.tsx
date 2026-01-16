8f1cfb425bc2c219b61b5bcafc9db5988e33033a
jhaharsh03 <harshj92002@gmail.com>
2026-01-16 23:20:51 +0530

    Added toss switch code

--- App.tsx
+++ App.tsx
@@ -1,118 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { Text, View, Pressable, StyleSheet } from "react-native";
+import { useState } from "react";
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function HomeScreen() { 
+    const [result, setResult] = useState("");
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+    const tossCoin = () => {
+      const randomNumber = Math.random(); 
+      setResult("Loading... ⏳");
+      setTimeout(() => {
+    
+    
+      if (randomNumber < 0.5) {
+            setResult("HEAD 🌞");
+          } else {
+            setResult("TAIL 🌙");
+          }
+        }, 1000);
+      };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+
+
+  return ( 
+    <View style={styles.container}> 
+      <Text style={styles.text}> 
+        Hello Everyone!
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+       <Pressable style={styles.button} onPress={tossCoin}>
+        <Text style={styles.tossText}>Click For Toss</Text>
+      </Pressable>
+
+       {result ? (
+        <Text style={styles.resultText}>Result: {result}</Text>
+      ) : null}
+    </View> 
+  ); 
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+export default HomeScreen
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
+  text: {
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "600",
   },
-  highlight: {
-    fontWeight: '700',
+  button: {
+    backgroundColor: "#007bff",
+    padding: 10,
+    borderRadius: 8,
+    marginTop: 15,
+  },
+  tossText: {
+    fontSize: 15,
+    fontWeight: "500",
+    color: "white",
+  },
+  resultText: {
+    marginTop: 15,
+    fontSize: 20,
+    fontWeight: "bold",
   },
 });
-
-export default App;
